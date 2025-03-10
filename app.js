e08b43178638b864c6f4bb6d23f5231822e337c4
Catherine Crenshaw <55335367+ccrenshawcodes@users.noreply.github.com>
2025-03-10 12:28:43 -0600

    lint and prettier

--- app.js
+++ app.js
@@ -1,10 +1,10 @@
 const express = require("express");
 const app = express();
-const usersRouter = require('./routes/usersRouter');
+const usersRouter = require("./routes/usersRouter");
 
 app.set("view engine", "ejs");
 app.use(express.urlencoded({ extended: true }));
-app.use('/', usersRouter);
+app.use("/", usersRouter);
 
 const PORT = process.env.PORT || 3000;
 app.listen(PORT, () => console.log(`express app listening on port ${PORT}`));
--- controllers/usersController.js
+++ controllers/usersController.js
@@ -1,25 +1,34 @@
 const usersStorage = require("../storages/usersStorage");
-const { body, validationResult } = require('express-validator');
+const { body, validationResult } = require("express-validator");
 
 const alphaErr = "must only contain letters";
-const lengthErr = "must be between 1 and 10 characters"
+const lengthErr = "must be between 1 and 10 characters";
 
 const validateUser = [
-  body("firstName").trim()
-    .isAlpha().withMessage(`first name ${alphaErr}`)
-    .isLength({ min: 1, max: 10}).withMessage(`Last name ${lengthErr}`),
-  body("lastName").trim()
-    .isAlpha().withMessage(`Last name ${alphaErr}`)
-    .isLength().withMessage(`Last name ${lengthErr}`),
-  body("userEmail").trim()
-    .isEmail().withMessage('Please enter a valid email.'),
-  body("age").trim()
+  body("firstName")
+    .trim()
+    .isAlpha()
+    .withMessage(`first name ${alphaErr}`)
+    .isLength({ min: 1, max: 10 })
+    .withMessage(`Last name ${lengthErr}`),
+  body("lastName")
+    .trim()
+    .isAlpha()
+    .withMessage(`Last name ${alphaErr}`)
+    .isLength()
+    .withMessage(`Last name ${lengthErr}`),
+  body("userEmail").trim().isEmail().withMessage("Please enter a valid email."),
+  body("age")
+    .trim()
     .optional({ values: "falsy" })
     //  check for being between 18-120. use .matches()?
-    .isNumeric().withMessage('Age must be a whole number'),
-  body("bio").trim()
+    .isNumeric()
+    .withMessage("Age must be a whole number"),
+  body("bio")
+    .trim()
     .optional({ values: "falsy" })
-    .isLength({ max: 200 }).withMessage('Your message must be less than 200 characters')
+    .isLength({ max: 200 })
+    .withMessage("Your message must be less than 200 characters"),
 ];
 
 exports.usersListGet = (req, res) => {
@@ -40,7 +49,7 @@ exports.usersCreatePost = [
   (req, res) => {
     const errors = validationResult(req);
     if (!errors.isEmpty()) {
-      return res.status(400).render('createUser', {
+      return res.status(400).render("createUser", {
         title: "Create user",
         errors: errors.array(),
       });
@@ -48,8 +57,8 @@ exports.usersCreatePost = [
 
     const { firstName, lastName, userEmail, age, bio } = req.body;
     usersStorage.addUser({ firstName, lastName, userEmail, age, bio });
-    res.redirect('/');
-  }
+    res.redirect("/");
+  },
 ];
 
 exports.usersUpdateGet = (req, res) => {
@@ -73,20 +82,28 @@ exports.usersUpdatePost = [
       });
     }
     const { firstName, lastName, userEmail, age, bio } = req.body;
-    usersStorage.updateUser(req.params.id, { firstName, lastName, userEmail, age, bio });
-    res.redirect('/');
-  }
+    usersStorage.updateUser(req.params.id, {
+      firstName,
+      lastName,
+      userEmail,
+      age,
+      bio,
+    });
+    res.redirect("/");
+  },
 ];
 
 exports.usersDeletePost = (req, res) => {
   usersStorage.deleteUser(req.params.id);
-  res.redirect('/');
-}
+  res.redirect("/");
+};
 
 exports.searchUsers = (req, res) => {
   const existingUsers = usersStorage.getUsers();
   const { userSearchInput } = req.query;
   const matches = [];
+
+  userSearchInput.toString();
 
   for (let i = 0; i < existingUsers.length; i++) {
     if (existingUsers[i].firstName.includes(userSearchInput)) {
@@ -102,5 +119,5 @@ exports.searchUsers = (req, res) => {
 
   res.render("searchResults", {
     results: matches,
-  })
+  });
 };
